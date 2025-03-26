a20202ba178c0d5db7901dfd3bd7ccbf1cec9807
kelton <kelton.tinho23@gmail.com>
2025-03-26 10:37:50 -0300

    Criando o projeto PDFScraper

---
+++ src/main/java/com/library/pdfscraper/PdfScraperApplication.java
@@ -0,0 +1,13 @@
+package com.library.pdfscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfScraperApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/library/pdfscraper/downloader/DownloadManager.java
@@ -0,0 +1,5 @@
+package com.library.pdfscraper.downloader;
+
+public class DownloadManager {
+
+}
